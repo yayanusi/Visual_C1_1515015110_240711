1fa0fa82f133cf25bec098f727e3671c59477a43
Kahar Nusi <yayanusi@gmail.com>
2017-04-07 02:39:27 +0800

    posttest 6
    posttest 6

---
+++ src/posttest6/koneksi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posttest6;
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class koneksi {
+    private static Connection con;
+    
+    public static Connection getConnection(){
+        try{
+            con = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/praktikum_visual","root","");
+            JOptionPane.showMessageDialog(null,"Koneksi Berhasil");
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Koneksi Gagal: "+e.getMessage());
+        }
+        return con;
+}
+}
